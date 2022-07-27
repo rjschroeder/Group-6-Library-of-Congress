17f656896571f9aaa348268caaaf949518ab2c1f
michaelalexdiaz <107277834+michaelalexdiaz@users.noreply.github.com>
2022-07-26 21:21:53 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+fetch('http://127.0.0.1:5501/06-Server-Side-APIs/01-Activities/27-Stu_Mini-Project/Main/index.html?').then(document.getElementsByClassName('btn').onclick = function(searchResult){
+    window.location.replace('http://127.0.0.1:5501/06-Server-Side-APIs/01-Activities/27-Stu_Mini-Project/Main/search-results.html');
+});
+console.log("hi");
